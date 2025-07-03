96bf144bd034b2740822f474d319814b4ce86c9c
EL_ZOZ <EL_ZOZ@ELHOSARY>
2025-07-03 20:31:43 +0300

    Add project files.

---
+++ week1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace week1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of small caarpets : ");
+            int numSmall = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Number of large caarpets : ");
+            int numLarge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("price per small room : $25 ");
+            Console.WriteLine("price per large room : $35 ");
+
+            int cost = numSmall * 25 + numLarge * 35;
+            Console.WriteLine($"Cost : ${cost} ");
+
+            double tax = cost * 0.06;
+            Console.WriteLine($"Tax : ${tax} ");
+
+            Console.WriteLine($"Total estimate : ${cost + tax} ");
+            Console.WriteLine("This estimate is valid for 30 days ");
+
+
+
+
+        }
+    }
+}
